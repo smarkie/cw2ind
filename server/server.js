aa7fbc0f99532d5085f5d4d253c88ffc7ff68d71
Smith Dbritto <75953565+smarkie@users.noreply.github.com>
2022-02-18 05:03:10 +0000

    test solution 1

--- server/server.js
+++ server/server.js
@@ -90,7 +90,7 @@ MongoClient.connect('mongodb+srv://smith:1234@cw2ind.0jm60.mongodb.net/test', (e
 })
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
