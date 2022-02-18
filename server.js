069c2cd247a5d2f702413eedfca7d4ea8c56b1f7
Smith Dbritto <75953565+smarkie@users.noreply.github.com>
2022-02-18 16:17:59 +0000

    port change to 3000

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 var path = require("path");
 var fs = require("fs");
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 
 
 app.use(express.json());
